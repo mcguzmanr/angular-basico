4dd953df1ad45d4d9c3ae87bfadbbcff18e94bd8
Marcelo Guzmán <mcguzmanr@gmail.com>
2021-03-07 13:31:13 -0500

    fin seccion 5

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: "Doctor Weas",
+    poder: 10000
+  }
+
+  constructor() {}
+}
